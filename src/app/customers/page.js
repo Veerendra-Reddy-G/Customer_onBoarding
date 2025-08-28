101d7b549625f6e9e1fa738f9bbeea651d7b21a1
Veerendra Reddy Gudimetla <veerendra.reddy@logituit.com>
2025-08-28 15:50:51 +0530

    second  commit

--- src/app/customers/page.js
+++ src/app/customers/page.js
@@ -1,17 +1,15 @@
 'use client';
 
 //import AuthGuard from '@/components/AuthGuard';
-import AuthGuard from '../components/Customers/AuthGuard';
-
 import CustomerList from '../components/Customers/CustomerList';
 
 export default function Customers() {
   return (
-     <AuthGuard>
+    //  <AuthGuard>
       <div>
         <h1>Customer Management</h1>
         <CustomerList />
       </div>
-     </AuthGuard>
+    //  </AuthGuard>
   );
 }
